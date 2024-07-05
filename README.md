17dcce2d204e6305798e1976a23ee8da2486f184
Max (Lenovo) <max.murphy11@gmail.com>
2024-07-05 13:54:35 -0400

    Adds working xbox_emulator_server_with_mouse.c, which compiles using gcc.

--- README.md
+++ README.md
@@ -1,38 +1,42 @@
 # Input Utilities #
-Simple compilable executables written in `c` for emulating keyboard/mouse inputs (Windows 11).  
 
-## Contents ##  
-Each `.c` file should have everything in it that's needed to compile.  
+Simple compilable executables written in `c` for emulating keyboard/mouse inputs (Windows 11).
+
+## Contents ##
+Each `.c` file should have everything in it that's needed to compile.
 * [Controller Emulator Server](#xbox-controller-emulator-server)
+* [Controller Emulator Server (With Mouse Port)](#xbox-controller-emulator-server-with-mouse)
 * [Key Event Listener (Debugging)](#key-event-listener)
 
+### XBOX Controller Emulator Server With Mouse ###
 
-### XBOX Controller Emulator Server ###  
+[Return](#contents)
 
-[Return](#contents)  
+This application creates a background local loopback server so you can send principled "messages" to the server over TCP/IP on your local device; the messages are translated by this server so that they look like actual keypresses from an XBOX controller, which should be compatible with most 3rd-party applications that you might want to play. So in a nutshell this is very useful if, for example, you are decoding device streams from EMG or other neural data sources in MATLAB or Python, and you want to handle all math and data processing in those higher-level languages. This allows separation/abstraction of the part where you have to generate the "physical key clicks" etc. so that other applications can see your decoded output.
 
-This application creates a background local loopback server so you can send principled "messages" to the server over TCP/IP on your local device; the messages are translated by this server so that they look like actual keypresses from an XBOX controller, which should be compatible with most 3rd-party applications that you might want to play. So in a nutshell this is very useful if, for example, you are decoding device streams from EMG or other neural data sources in MATLAB or Python, and you want to handle all math and data processing in those higher-level languages. This allows separation/abstraction of the part where you have to generate the "physical key clicks" etc. so that other applications can see your decoded output.    
-
-#### Server Application #### 
-To compile the server application, run the following command from an `MSYS64` terminal in the root folder of this repository:  
+#### Server Application ####
+To compile the server application, run the following command from an `MSYS64` terminal in the root folder of this repository:
 ```bash
 gcc src/xbox_controller_emulator.c -o xbox_emulator_server.exe -lws2_32
-``` 
-To run the compiled application, from the same terminal you can simply then run:  
+```
+To run the compiled application, from the same terminal you can simply then run:
 ```bash
 ./xbox_emulator_server.exe
 ```
-(Or just double-click the compiled executable).  
-You may receive a security prompt to open a port--this is exposed on local loopback; make sure to allow it.  
+(Or just double-click the compiled executable).
+You may receive a security prompt to open a port--this is exposed on local loopback; make sure to allow it.
 
-##### Message API #####  
-The setup is simple. Currently, it's hard-coded to connect on port `6053` so you will follow the general steps of:  
-1. Create a TCP/IP client socket connection to port `6053` on `localhost` (`127.0.0.1`).  
-2. Send `\r\n`-terminated `char` messages to the socket with structure: `<button><0 or 1>`.  
+##### Message API #####
+The setup is simple. Currently, it's hard-coded to connect on port `6053` for digital inputs and port `6054` for analog (mouse) inputs.
 
-The first character in the message denotes which button to press or release. The second character (`'0'` or `'1'`) denotes whether to press or release the button (respectively; yes, `'0'` goes with "press").  See the table below for button keys:  
+#### Digital Input Messages ####
+1. Create a TCP/IP client socket connection to port `6053` on `localhost` (`127.0.0.1`).
+2. Send `\r\n`-terminated `char` messages to the socket with structure: `<button><0 or 1>`.
+
+The first `char` of the message must be one of the following, indicating the button to press:
+
 | Button | Character |
-| ----: | :-------  |
+| ---    | ---       |
 | `A` | `'a'` or `'A'`|
 | `B` | `'b'` or `'B'`|
 | `X` | `'x'` or `'X'`|
@@ -44,15 +48,15 @@ The first character in the message denotes which button to press or release. The
 | `DPAD LEFT`   | `'4'` |
 | `DPAD RIGHT`  | `'6'` |
 
-_(The `DPAD` button `<-->` character mapping is related to the layout of the numbers on a standard keyboard numpad.)_    
+_(The `DPAD` button `<-->` character mapping is related to the layout of the numbers on a standard keyboard numpad.)_
 
-I may add more buttons depending if anybody uses/requests them.  
+The second `char` should be either `'0'` (key press) or `'1'` (key release).
 
-##### Example Usage #####  
-Below is some example MATLAB code I've confirmed works to make Megaman move back and forth and charge/discharge his hand cannon in MegaManX running in SNES9x while I run this emulator server in the background:  
+##### Example Usage #####
+Below is some example MATLAB code I've confirmed works to make Megaman move back and forth and charge/discharge his hand cannon in MegaManX running in SNES9x while I run this emulator server in the background:
 ```matlab
 client = tcpclient("127.0.0.1", 6053);
-writeline(client, 'y0'); % Press (and hold) "Y" 
+writeline(client, 'y0'); % Press (and hold) "Y"
 pause(0.020);
 writeline(client, '60'); % Press (and hold) DPAD Right
 pause(1.5);
@@ -70,32 +74,119 @@ pause(0.020);
 writeline(client, 'Y1'); % FIRE!!!! (Release "Y")
 ```
 
-#### Test Application ####
-To compile an application that you can run right away to confirm if the server is working as intended, run the following from an `MSYS64` terminal in the root folder of this repository:  
+### XBOX Controller Emulator Server With Mouse ###
+
+[Return](#contents)
+
+This application creates a background local loopback server so you can send principled "messages" to the server over TCP/IP on your local device; the messages are translated by this server so that they look like actual keypresses from an XBOX controller, which should be compatible with most 3rd-party applications that you might want to play. So in a nutshell this is very useful if, for example, you are decoding device streams from EMG or other neural data sources in MATLAB or Python, and you want to handle all math and data processing in those higher-level languages. This allows separation/abstraction of the part where you have to generate the "physical key clicks" etc. so that other applications can see your decoded output.
+
+#### Server Application ####
+To compile the server application, run the following command from an `MSYS64` terminal in the root folder of this repository:
 ```bash
-gcc src/xbox_emulator__simple_test.c -o xbox_emulator__simple_test
+gcc src/xbox_controller_emulator_with_mouse.c -o xbox_emulator_server_with_mouse.exe -lws2_32
 ```
-To run  
+To run the compiled application, from the same terminal you can simply then run:
+```bash
+./xbox_emulator_server_with_mouse.exe
+```
+(Or just double-click the compiled executable).
+You may receive a security prompt to open a port--this is exposed on local loopback; make sure to allow it.
+
+##### Message API #####
+The setup is simple. Currently, it's hard-coded to connect on port `6053` for digital inputs and port `6054` for analog (mouse) inputs.
+
+#### Digital Input Messages ####
+1. Create a TCP/IP client socket connection to port `6053` on `localhost` (`127.0.0.1`).
+2. Send `\r\n`-terminated `char` messages to the socket with structure: `<button><0 or 1>`.
+
+The first `char` of the message must be one of the following, indicating the button to press:
+
+| Button | Character |
+| ---    | ---       |
+| `A` | `'a'` or `'A'`|
+| `B` | `'b'` or `'B'`|
+| `X` | `'x'` or `'X'`|
+| `Y` | `'y'` or `'Y'`|
+| `L` | `'l'` or `'L'`|
+| `R` | `'r'` or `'R'`|
+| `DPAD UP`     | `'8'` |
+| `DPAD DOWN`   | `'2'` |
+| `DPAD LEFT`   | `'4'` |
+| `DPAD RIGHT`  | `'6'` |
+
+_(The `DPAD` button `<-->` character mapping is related to the layout of the numbers on a standard keyboard numpad.)_
+
+The second `char` should be either `'0'` (key press) or `'1'` (key release).
+
+##### Example Usage #####
+Below is some example MATLAB code I've confirmed works to make Megaman move back and forth and charge/discharge his hand cannon in MegaManX running in SNES9x while I run this emulator server in the background:
+```matlab
+client = tcpclient("127.0.0.1", 6053);
+writeline(client, 'y0'); % Press (and hold) "Y"
+pause(0.020);
+writeline(client, '60'); % Press (and hold) DPAD Right
+pause(1.5);
+writeline(client, '61'); % Stop running Right
+pause(0.020);
+writeline(client, '40'); % Start running Left
+pause(1.5);
+writeline(client, '41'); % Stop running Left
+pause(0.020);
+% Turn right:
+writeline(client, '61');
+pause(0.020);
+writeline(client, '60');
+pause(0.020); 
+writeline(client, 'Y1'); % FIRE!!!! (Release "Y")
+```
+
+### Analog Input Messages
+
+1. Create a TCP/IP client socket connection to port `6054` on `localhost` (`127.0.0.1`).
+2. Send messages to the socket with one of the following structures:
+
+- **Analog Movement**: `<direction>,<dx>,<dy>`
+  - `direction`: 'u', 'd', 'l', 'r' for up, down, left, right movement.
+  - `dx`, `dy`: Integer values for movement along the x and y axes.
+- **Mouse Clicks**: `click,<button>`
+  - `button`: 'left' or 'right'.
+- **Mouse Wheel Scroll**: `scroll,<direction>`
+  - `direction`: 'up' or 'down'.
+
+#### Example Usage
+Below is some example MATLAB code to simulate analog stick movement, mouse clicks, and mouse wheel scrolls:
+```matlab
+mouse = tcpclient("127.0.0.1", 6054);
+% Analog movement
+writeline(mouse, 'u,0,-250'); % Move up 250 pixels
+pause(0.1);
+writeline(mouse, 'd,0,250'); % Move down 250 pixels
+pause(0.1);
+writeline(mouse, 'l,-250,0'); % Move left 250 pixels
+pause(0.1);
+writeline(mouse, 'r,250,0'); % Move right 250 pixels
+pause(0.1);
+% Mouse clicks
+writeline(mouse, 'click,left'); % Left mouse click
+pause(0.1);
+writeline(mouse, 'click,right'); % Right mouse click
+pause(0.1);
+% Mouse wheel scroll
+writeline(mouse, 'scroll,up'); % Scroll up
+pause(0.1);
+writeline(mouse, 'scroll,down'); % Scroll down
+pause(0.1);
+
+#### Test Application ####
+To compile an application that you can run right away to confirm if the server is working as intended, run the following from an `MSYS64` terminal in the root folder of this repository:
+```bash
+gcc src/xbox_emulator__simple_test.c -o xbox_emulator__simple_test -lws2_32
+```
+To run
 ```bash
 ./xbox_emulator__simple_test.exe
-``` 
-(Or double-click the compiled executable).   
-
----
-
-### Key Event Listener ### 
-
-[Return](#contents)  
-
-If you're not sure what the windows keycodes should be enumerated as, then run this to get them to print out into a terminal.  
-
-#### Listener Application ####  
-To compile
-```bash
-gcc src/key_event_listener.c -o key_event_listener
 ```
-To run  
-```bash
-./key_event_listener.exe
-``` 
-(Or double-click the compiled executable).   
+(Or double-click the compiled executable).
+```
+
+This updated README includes instructions for handling analog input messages on port 6054 and provides example usage for sending these messages from MATLAB. The new analog messages allow you to specify direction and movement amounts, simulating analog stick movement on the Xbox controller.
---
+++ src/xbox_controller_emulator_with_mouse.c
@@ -0,0 +1,356 @@
+#include <winsock2.h>
+#include <stdio.h>
+#include <signal.h>
+#include <windows.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#define XBOX_CONTROLLER_EMULATOR_PORT 6053
+#define XBOX_ANALOG_EMULATOR_PORT 6054
+
+// Global flag for signal handling
+volatile sig_atomic_t flag = 0;
+INPUT input[3];
+POINT p;
+
+// Signal handler for SIGINT (Ctrl+C)
+void sigint_handler(int sig) {
+    flag = 1;
+}
+
+void disableMouseAcceleration() {
+    SystemParametersInfo(SPI_SETMOUSE, 0, NULL, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+}
+
+void enableMouseAcceleration() {
+    SystemParametersInfo(SPI_SETMOUSE, 1, NULL, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+}
+
+void setKey(char key, char updown) {
+    switch (key)
+    {
+        case 'a':
+        case 'A': {
+            input[updown - '0'].ki.wVk = 'A';
+            break;
+        }
+        case 'b':
+        case 'B': {
+            input[updown - '0'].ki.wVk = 'B';
+            break;
+        }
+        case 'x':
+        case 'X': {
+            input[updown - '0'].ki.wVk = 'X';
+            break;
+        }
+        case 'y':
+        case 'Y': {
+            input[updown - '0'].ki.wVk = 'Y';
+            break;
+        }
+        case 'l':
+        case 'L': {
+            input[updown - '0'].ki.wVk = 'L';
+            break;
+        }
+        case 'r':
+        case 'R': {
+            input[updown - '0'].ki.wVk = 'R';
+            break;
+        }
+        case '4': { // left
+            input[updown - '0'].ki.wVk = 0x25;
+            break;
+        }
+        case '6': { // right
+            input[updown - '0'].ki.wVk = 0x27;
+            break;
+        }
+        case '2': { // down
+            input[updown - '0'].ki.wVk = 0x28;
+            break;
+        }
+        case '8': { // up
+            input[updown - '0'].ki.wVk = 0x26;
+            break;
+        }
+    }    
+    SendInput(1, &input[updown - '0'], sizeof(INPUT));
+}
+
+void setAnalog(int dx, int dy) {
+    if (GetCursorPos(&p)) {
+        printf("Current Position: (%d, %d)\n", p.x, p.y);
+    }
+
+    input[2].type = INPUT_MOUSE;
+    input[2].mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+    input[2].mi.dx = (p.x + dx) * 65535 / GetSystemMetrics(SM_CXSCREEN);
+    input[2].mi.dy = (p.y + dy) * 65535 / GetSystemMetrics(SM_CYSCREEN);
+    input[2].mi.dwExtraInfo = 0;
+    input[2].mi.mouseData = 0;
+    input[2].mi.time = 0;
+    SendInput(1, &input[2], sizeof(INPUT));
+
+    if (GetCursorPos(&p)) {
+        printf("New Position: (%d, %d)\n", p.x, p.y);
+    }
+}
+
+void mouseClick(const char *button) {
+    input[2].type = INPUT_MOUSE;
+    input[2].mi.dwFlags = (strcmp(button, "left") == 0) ? MOUSEEVENTF_LEFTDOWN : MOUSEEVENTF_RIGHTDOWN;
+    SendInput(1, &input[2], sizeof(INPUT));
+
+    input[2].mi.dwFlags = (strcmp(button, "left") == 0) ? MOUSEEVENTF_LEFTUP : MOUSEEVENTF_RIGHTUP;
+    SendInput(1, &input[2], sizeof(INPUT));
+}
+
+void mouseOn(const char *button) {
+    input[2].type = INPUT_MOUSE;
+    input[2].mi.dwFlags = (strcmp(button, "left") == 0) ? MOUSEEVENTF_LEFTDOWN : MOUSEEVENTF_RIGHTDOWN;
+    SendInput(1, &input[2], sizeof(INPUT));
+}
+
+void mouseOff(const char *button) {
+    input[2].type = INPUT_MOUSE;
+    input[2].mi.dwFlags = (strcmp(button, "left") == 0) ? MOUSEEVENTF_LEFTUP : MOUSEEVENTF_RIGHTUP;
+    SendInput(1, &input[2], sizeof(INPUT));
+}
+
+void mouseScroll(const char *direction) {
+    input[2].type = INPUT_MOUSE;
+    input[2].mi.dwFlags = MOUSEEVENTF_WHEEL;
+    input[2].mi.mouseData = (strcmp(direction, "up") == 0) ? WHEEL_DELTA : -WHEEL_DELTA;
+    SendInput(1, &input[2], sizeof(INPUT));
+}
+
+int main() {
+    // Register signal handler for SIGINT (Ctrl+C)
+    signal(SIGINT, sigint_handler);
+    memset(input, 0, sizeof(input)); // Clear input structure
+
+    // Set up the input structure for key press
+    input[0].type = INPUT_KEYBOARD;
+    input[0].ki.dwFlags = 0; // 0 for key press
+
+    // Set up the input structure for key release
+    input[1].type = INPUT_KEYBOARD;
+    input[1].ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+
+    // Disable mouse acceleration
+    disableMouseAcceleration();
+
+    // Initialize Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup failed\n");
+        return 1;
+    }
+
+    // Create socket for digital inputs
+    SOCKET listenSocketDigital = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listenSocketDigital == INVALID_SOCKET) {
+        printf("Error creating socket: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    // Bind socket for digital inputs
+    struct sockaddr_in serverAddrDigital;
+    serverAddrDigital.sin_family = AF_INET;
+    serverAddrDigital.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddrDigital.sin_port = htons(XBOX_CONTROLLER_EMULATOR_PORT);
+
+    if (bind(listenSocketDigital, (struct sockaddr *)&serverAddrDigital, sizeof(serverAddrDigital)) == SOCKET_ERROR) {
+        printf("Bind failed with error: %d\n", WSAGetLastError());
+        closesocket(listenSocketDigital);
+        WSACleanup();
+        return 1;
+    }
+
+    // Listen for incoming connections on digital input port
+    if (listen(listenSocketDigital, SOMAXCONN) == SOCKET_ERROR) {
+        printf("Listen failed with error: %d\n", WSAGetLastError());
+        closesocket(listenSocketDigital);
+        WSACleanup();
+        return 1;
+    }
+
+    // Create socket for analog inputs
+    SOCKET listenSocketAnalog = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listenSocketAnalog == INVALID_SOCKET) {
+        printf("Error creating socket: %d\n", WSAGetLastError());
+        closesocket(listenSocketDigital);
+        WSACleanup();
+        return 1;
+    }
+
+    // Bind socket for analog inputs
+    struct sockaddr_in serverAddrAnalog;
+    serverAddrAnalog.sin_family = AF_INET;
+    serverAddrAnalog.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddrAnalog.sin_port = htons(XBOX_ANALOG_EMULATOR_PORT);
+
+    if (bind(listenSocketAnalog, (struct sockaddr *)&serverAddrAnalog, sizeof(serverAddrAnalog)) == SOCKET_ERROR) {
+        printf("Bind failed with error: %d\n", WSAGetLastError());
+        closesocket(listenSocketDigital);
+        closesocket(listenSocketAnalog);
+        WSACleanup();
+        return 1;
+    }
+
+    // Listen for incoming connections on analog input port
+    if (listen(listenSocketAnalog, SOMAXCONN) == SOCKET_ERROR) {
+        printf("Listen failed with error: %d\n", WSAGetLastError());
+        closesocket(listenSocketDigital);
+        closesocket(listenSocketAnalog);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("XBOX Controller Emulator Running on Ports %d (Digital) and %d (Analog)!\n", XBOX_CONTROLLER_EMULATOR_PORT, XBOX_ANALOG_EMULATOR_PORT);
+
+    // Accept incoming connections
+    SOCKET clientSocketDigital;
+    struct sockaddr_in clientAddrDigital;
+    int clientAddrLenDigital = sizeof(clientAddrDigital);
+
+    clientSocketDigital = accept(listenSocketDigital, (struct sockaddr *)&clientAddrDigital, &clientAddrLenDigital);
+    if (clientSocketDigital == INVALID_SOCKET) {
+        printf("Accept failed with error: %d\n", WSAGetLastError());
+        closesocket(listenSocketDigital);
+        closesocket(listenSocketAnalog);
+        WSACleanup();
+        return 1;
+    }
+
+    // Set client socket to non-blocking mode
+    u_long mode = 1;
+    ioctlsocket(clientSocketDigital, FIONBIO, &mode);
+
+    SOCKET clientSocketAnalog;
+    struct sockaddr_in clientAddrAnalog;
+    int clientAddrLenAnalog = sizeof(clientAddrAnalog);
+
+    clientSocketAnalog = accept(listenSocketAnalog, (struct sockaddr *)&clientAddrAnalog, &clientAddrLenAnalog);
+    if (clientSocketAnalog == INVALID_SOCKET) {
+        printf("Accept failed with error: %d\n", WSAGetLastError());
+        closesocket(listenSocketDigital);
+        closesocket(listenSocketAnalog);
+        WSACleanup();
+        return 1;
+    }
+
+    // Set client socket to non-blocking mode
+    ioctlsocket(clientSocketAnalog, FIONBIO, &mode);
+
+    printf("XBOX Controller connected.\n");
+
+    char buffer[5]; // Buffer to store received message
+    char analogBuffer[100]; // Buffer for analog input
+
+    while (!flag) {
+        // Check for digital input
+        int bytesReceivedDigital = recv(clientSocketDigital, buffer, sizeof(buffer), 0);
+        if (bytesReceivedDigital > 0) {
+            // Extract the key from the received message
+            char key = buffer[0];
+            char updown = buffer[1];
+            
+            // Set the key for input
+            setKey(key, updown);
+        }
+        else if (bytesReceivedDigital == 0) {
+            printf("Client disconnected from digital input\n");
+            break;
+        }
+        else if (bytesReceivedDigital == SOCKET_ERROR && WSAGetLastError() != WSAEWOULDBLOCK) {
+            printf("Receive failed with error: %d\n", WSAGetLastError());
+            break;
+        }
+
+        // Check for analog input
+        int bytesReceivedAnalog = recv(clientSocketAnalog, analogBuffer, sizeof(analogBuffer), 0);
+        if (bytesReceivedAnalog > 0) {
+            // Determine the type of analog message
+            char type[10];
+            sscanf(analogBuffer, "%9[^,]", type);
+
+            if (strcmp(type, "u") == 0 || strcmp(type, "d") == 0 || strcmp(type, "l") == 0 || strcmp(type, "r") == 0) {
+                // Expected format: "<direction>,<dx>,<dy>\n"
+                char direction[2];
+                int dx, dy;
+                sscanf(analogBuffer, "%1s,%d,%d", direction, &dx, &dy);
+                
+                // Debug print statements
+                printf("Analog Input Received: Direction: %s, dx: %d, dy: %d\n", direction, dx, dy);
+                
+                // Set the analog input
+                setAnalog(dx, dy);
+            } else if (strcmp(type, "click") == 0) {
+                // Expected format: "click,<button>\n"
+                char button[6];
+                sscanf(analogBuffer, "click,%5s", button);
+
+                // Debug print statements
+                printf("Mouse Click Received: Button: %s\n", button);
+
+                // Set the mouse click
+                mouseClick(button);
+            } else if (strcmp(type, "scroll") == 0) {
+                // Expected format: "scroll,<direction>\n"
+                char direction[6];
+                sscanf(analogBuffer, "scroll,%5s", direction);
+
+                // Debug print statements
+                printf("Mouse Scroll Received: Direction: %s\n", direction);
+
+                // Set the mouse scroll
+                mouseScroll(direction);
+            } else if (strcmp(type, "on") == 0) {
+                // Expected format: "hold,<button>\n"
+                char button[6];
+                sscanf(analogBuffer, "on,%5s", button);
+
+                // Debug print statements
+                printf("Mouse ON Received: Button: %s\n", button);
+
+                // Set the mouse button as clicked/dragging
+                mouseOn(button);
+            }  else if (strcmp(type, "off") == 0) {
+                // Expected format: "hold,<button>\n"
+                char button[6];
+                sscanf(analogBuffer, "off,%5s", button);
+
+                // Debug print statements
+                printf("Mouse OFF Received: Button: %s\n", button);
+
+                // Set the mouse click
+                mouseOff(button);
+            }
+        }
+        else if (bytesReceivedAnalog == 0) {
+            printf("Client disconnected from analog input\n");
+            break;
+        }
+        else if (bytesReceivedAnalog == SOCKET_ERROR && WSAGetLastError() != WSAEWOULDBLOCK) {
+            printf("Receive failed with error: %d\n", WSAGetLastError());
+            break;
+        }
+
+        Sleep(10); // Add a small delay to prevent tight loop
+    }
+
+    // Enable mouse acceleration
+    enableMouseAcceleration();
+
+    // Cleanup
+    closesocket(clientSocketDigital);
+    closesocket(clientSocketAnalog);
+    closesocket(listenSocketDigital);
+    closesocket(listenSocketAnalog);
+    WSACleanup();
+
+    return 0;
+}
